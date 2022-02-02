159c1d7460822c4eb61de8cb79c2b5ddbf87e889
William <Williamaxb@gmail.com>
2022-02-02 11:15:42 +0100

    new header component, component added to index

--- recruitment-app/pages/index.tsx
+++ recruitment-app/pages/index.tsx
@@ -2,13 +2,16 @@ import { useDeno } from 'aleph/react'
 import React from 'react'
 import Logo from '~/components/logo.tsx'
 import useCounter from '~/lib/useCounter.ts'
+import Header from '~/components/header.tsx'
 
 export default function Home() {
   const [count, isSyncing, increase, decrease] = useCounter()
   const version = useDeno(() => Deno.version.deno)
 
   return (
+    
     <div className="page">
+      <Header></Header>
       <head>
         <title>Hello World - Aleph.js</title>
         <link rel="stylesheet" href="../style/index.css" />
