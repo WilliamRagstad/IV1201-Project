f8f697abbdb70cb9c9a54f1acf668126e06617b4
William <Williamaxb@gmail.com>
2022-02-03 11:01:25 +0100

    changed index to login page

--- recruitment-app/pages/index.tsx
+++ recruitment-app/pages/index.tsx
@@ -1,15 +1,12 @@
 import { useDeno } from 'aleph/react'
 import React from 'react'
-import useCounter from '~/lib/useCounter.ts'
 import Header from '~/components/header.tsx'
 import Footer from '~/components/footer.tsx'
 
 export default function Home() {
-  const [count, isSyncing, increase, decrease] = useCounter()
   const version = useDeno(() => Deno.version.deno)
-
+  //Check if user is logged in and return proper page and if not return log in page
   return (
-    
     <div className="page">
       <Header></Header>
       <head>
@@ -20,51 +17,15 @@ export default function Home() {
       <h1>Welcome to use <strong>Amusement Park Recruiting</strong>!</h1>
       <h2>Apply for work here</h2>
       <form action="/" method="post" className="signup_form">
-        <div className="personal_information">
-        First Name:
-          <label className="txt_field">
-            <input type="text" name="firstname"/> 
-          </label>
-            Last Name:
-          <label className="txt_field">
-            <input type="text" name="lastname"/> 
-          </label>
-            Person Number: 
-          <label className="txt_field">
-            <input type="number" name="personnumber"/> 
-          </label>
           Email:
           <label className="txt_field">
-            
             <input type="email" name="email"/> 
           </label>
-        </div>
-        <div className="expertise">
-          <label className="check_box">
-            <input type="checkbox" name="expertise1"/> 
-            Customer Support
-          </label> 
-          <label className="check_box">
-            <input type="checkbox" name="expertise2"/> 
-            IT
+          Password:
+          <label className="txt_field">
+            <input type="password" name="password"/> 
           </label>
-          <label className="check_box">
-            <input type="checkbox" name="expertise3"/> 
-            Food and Restaurant
-          </label>
-          <label className="check_box">
-            <input type="checkbox" name="expertise4"/> 
-            Administration
-          </label>   
-        </div>
-        <div className="availability_dates">
-          <label>
-            Dates for work:  
-            <input type="date" name="startdate"/> 
-            <input type="date" name="enddate"/> 
-          </label>  
-        </div>    
-        <input type="submit" value="Apply" class="button"/>
+        <input type="submit" value="Login" class="button"/>
       </form>
       <Footer></Footer>
     </div>
