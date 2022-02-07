9abdb658aadc0e1d2924bbe1f45152406c485ec6
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-02-07 20:44:48 +0100

    Finalized User controller

--- recruitment-api/controller/UserController.ts
+++ recruitment-api/controller/UserController.ts
@@ -1,11 +1,15 @@
 import { Context } from "https://deno.land/x/oak/mod.ts";
 import IController from "../lib/mvc-manager/IController.ts";
+import { bodyMappingJSON, created } from "../lib/mvc-manager/ControllerUtils.ts";
+import User from "../model/User.ts";
 
 export default class UserController implements IController {
 	path = "/user";
 
-	post({ response }: Context): void {
-		response.body = "Hello World from ExampleController/post";
+	async post({ request, response }: Context): Promise<void> {
+		const user = await bodyMappingJSON(request, User);
+		// TODO: Save user to database
+		created(response, `User ${user.firstName} was successfully created`);
 	}
 
 }
