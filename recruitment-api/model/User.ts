3a351b9f34f10d913c6f07a517c867f78bd048e6
William <Williamaxb@gmail.com>
2022-02-08 15:54:14 +0100

    Updated Model

--- recruitment-api/model/User.ts
+++ recruitment-api/model/User.ts
@@ -1,5 +1,3 @@
-import DBHandler from "../lib/database-manager/DBHandler.ts";
-
 /**
  * User DTO
  */
@@ -24,26 +22,5 @@ export default class User {
 		this.city = city;
 		this.phone = phone;
 	}
-	PostUser():void {
-        DBHandler.postUser(this);
-    }
 }
 
-/**
- * Competence Application DTO
- */
-export class CompetenceApplication {
-    competence1?: boolean;
-    competence2?: boolean;
-    competence3?: boolean;
-
-    constructor(competence1: boolean, competence2: boolean, competence3: boolean){
-        this.competence1 = competence1;
-        this.competence2 = competence2;
-        this.competence3 = competence3;
-    }
-
-    submitApplication():void {
-        //connect to database and insert application
-    }
-}
