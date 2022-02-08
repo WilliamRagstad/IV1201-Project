c2f7ecede681f23bfacf8b72a225c7cc40172657
Ludvig Lindén <ludvig978@gmail.com>
2022-02-08 15:26:20 +0100

    Added default html layout page, changed some styling and layout

--- recruitment-app/components/header.tsx
+++ recruitment-app/components/header.tsx
@@ -6,13 +6,6 @@ export default function Header() {
   //If logged in, dont show sign up
   return (
     <div className="header">
-      <head>
-        <link rel="stylesheet" href="../style/index.css" />
-        <head>
-        <title>Amusement Park Recruiting</title>
-        <link rel="stylesheet" href="../style/index.css" />
-      </head>
-      </head>
       <p className="logo"><Logo /></p>
       <p className="nav">
         <a href="/">Home</a>
