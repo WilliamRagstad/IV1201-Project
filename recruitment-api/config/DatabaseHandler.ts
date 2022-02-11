1cbb993e7abf10a2fe079f87756a9b711fdf9b26
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-02-11 12:29:12 +0100

    Update DatabaseHandler.ts

--- recruitment-api/config/DatabaseHandler.ts
+++ recruitment-api/config/DatabaseHandler.ts
@@ -6,46 +6,45 @@ import { credentials } from "./DatabaseCredentials.ts";
  * manage the communication with the database.
  */
 export default class DatabaseHandler {
+  private static instance: DatabaseHandler;
+  private client: Client;
 
-	private static instance: DatabaseHandler;
-	private client: Client;
+  private constructor() {
+    this.client = new Client(credentials);
+  }
 
-	private constructor() {
-		this.client = new Client(credentials);
-	}
+  /**
+   * Get the database singleton instance.
+   * @returns The singleton instance used to communicate with the database.
+   */
 
-	/**
-	 * Get the database singleton instance.
-	 * @returns The singleton instance used to communicate with the database.
-	 */
+  public static getInstance(): DatabaseHandler {
+    if (!DatabaseHandler.instance) {
+      DatabaseHandler.instance = new DatabaseHandler();
+    }
+    return DatabaseHandler.instance;
+  }
 
-	public static getInstance(): DatabaseHandler {
-		if (!DatabaseHandler.instance) {
-			DatabaseHandler.instance = new DatabaseHandler();
-		}
-		return DatabaseHandler.instance;
-	}
+  /**
+   * Connect to the database.
+   */
+  public async connect() {
+    if (!this.client.connected) await this.client.connect();
+  }
 
-	/**
-	 * Connect to the database.
-	 */
-	 public async connect() {
-		if (!this.client.connected) await this.client.connect();
-	}
+  /**
+   * Disconnect from the database and delete all non-persistent data.
+   */
+  public async disconnect() {
+    if (this.client.connected) await this.client.end();
+  }
 
-	/**
-	 * Disconnect from the database and delete all non-persistent data.
-	 */
-	 public async disconnect() {
-		if (this.client.connected) await this.client.end();
-	}
-
-	/**
-	 * Send a query to the database.
-	 * @param query The query to send to the database.
-	 * @returns The result of the query.
-	 */
-	public query(query: string) {
-		return this.client.queryArray(query);
-	}
+  /**
+   * Send a query to the database.
+   * @param query The query to send to the database.
+   * @returns The result of the query.
+   */
+  public query(query: string) {
+    return this.client.queryArray(query);
+  }
 }
