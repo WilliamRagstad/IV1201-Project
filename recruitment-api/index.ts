fb8ffd976c948875f683363d8ed2a1d8bc516766
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-02-11 12:25:58 +0100

    Connect to database with DatabaseHandler

--- recruitment-api/index.ts
+++ recruitment-api/index.ts
@@ -1,4 +1,5 @@
 import { AppMode, Knight } from "https://deno.land/x/knight@2.0.3/mod.ts";
+import DatabaseHandler from "./config/DatabaseHandler.ts";
 import ExampleController from "./controller/ExampleController.ts";
 import UserController from "./controller/UserController.ts";
 
@@ -7,9 +8,11 @@ const app = Knight.createApi([
   new ExampleController(),
   new UserController(),
 ]);
+DatabaseHandler.getInstance().connect();
 
 console.log(
   AppMode[Knight.getMode()] +
     " Server ready on http://localhost:8000",
 );
 await app.listen({ port: 8000 });
+DatabaseHandler.getInstance().disconnect();
