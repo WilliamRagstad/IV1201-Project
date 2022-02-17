2c847e7e7c565f0b50c68d47d7d2769e845657ff
William <Williamaxb@gmail.com>
2022-02-17 11:51:00 +0100

    Added Availability DTO

---
+++ recruitment-api/model/Availability.ts
@@ -0,0 +1,11 @@
+
+
+export default class Availability {
+	start_date: string;
+	end_date: string;
+
+	constructor(start_date: string, end_date: string) {
+		this.start_date = start_date;
+		this.end_date = end_date;
+	}
+}
