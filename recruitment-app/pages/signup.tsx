de35cd8ca6aaa3f1d791bc51fd5d211545336171
Ludvig Lindén <ludvig978@gmail.com>
2022-02-17 14:40:27 +0100

    Added hash function for form password

--- recruitment-app/pages/signup.tsx
+++ recruitment-app/pages/signup.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import DefaultPage from "../components/defaultpage.tsx";
+import { createHash } from "https://deno.land/std@0.77.0/hash/mod.ts";
+
+const hash = createHash("sha256");
 
 /**
  * The signup page.
  * @returns a page with the form to register to the recruitment app.
  */
 export default function Signup() {
+  function hashPassword(password: string) {
+    hash.update(password);
+    const hashed = hash.toString();
+    return hashed;
+  }
+  const submitForm: React.FormEventHandler = function (event: React.FormEvent<Element>) {
+    const password = (document.getElementById("password") as HTMLInputElement).value;
+    const hashed = hashPassword(password);
+    (document.getElementById("password") as HTMLInputElement).value = hashed;
+  }
+  
   return (
     <DefaultPage header="Sign up">
       <form
         action="http://localhost:8000/user"
         method="post"
         className="signup_form"
+        encType="multipart/form-data"
+        onSubmit={submitForm}
       >
         <div className="personal_information">
           First Name:
@@ -44,11 +60,11 @@ export default function Signup() {
           </label>
           Password:
           <label className="txt_field">
-            <input type="password" name="password" />
+            <input id="password" type="password" name="password" />
           </label>
         </div>
 
-        <input type="submit" value="Sign Up" className="button" />
+        <input type="submit" value="Sign Up" className="button"/>
       </form>
     </DefaultPage>
   );
