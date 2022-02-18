94bca77516929dd89da7ae6558b216ea334c9296
Ludvig Lindén <ludvig978@gmail.com>
2022-02-18 11:16:32 +0100

    Removed unnused textinputs and renamed to used ones

--- recruitment-app/pages/signup.tsx
+++ recruitment-app/pages/signup.tsx
@@ -40,31 +40,23 @@ export default function Signup() {
         <div className="personal_information">
           First Name:
           <label className="txt_field">
-            <input type="text" name="firstname" />
+            <input type="text" name="firstName" />
           </label>
           Last Name:
           <label className="txt_field">
-            <input type="text" name="lastname" />
+            <input type="text" name="lastName" />
           </label>
           Person Number:
           <label className="txt_field">
-            <input type="number" name="personnumber" />
-          </label>
-          Country:
-          <label className="txt_field">
-            <input type="text" name="country" />
-          </label>
-          City:
-          <label className="txt_field">
-            <input type="text" name="city" />
-          </label>
-          Phone:
-          <label className="txt_field">
-            <input type="number" name="phone" />
+            <input type="number" name="socialSecurityNumber" />
           </label>
           Email:
           <label className="txt_field">
             <input type="email" name="email" />
+          </label>
+          Username:
+          <label className="txt_field">
+            <input type="text" name="username" />
           </label>
           Password:
           <label className="txt_field">
