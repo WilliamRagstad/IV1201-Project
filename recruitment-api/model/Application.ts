d40105e768a3e537c21094fea59dae94ed911a54
Ludvig Lindén <ludvig978@gmail.com>
2022-02-21 13:22:51 +0100

    Changed CompetenceApplication model to Competence

--- recruitment-api/model/Application.ts
+++ recruitment-api/model/Application.ts
@@ -1,33 +1,31 @@
 import { Optional } from "https://deno.land/x/knight@2.0.3/mod.ts";
+import Availability from "./Availability.ts";
+import Competence from "./Competence.ts";
 
 export default class Application {
     person_id: number;
     name: string;
     surname: string;
     email: string;
-    competence_id: number;
-    years_of_experience: number;
-    from_date: string;
-    to_date: string;
+    @Optional()
+    competence?: Competence[];
+    @Optional()
+    availability?: Availability[];
 
     constructor(
         person_id: number,
         name: string,
         surname: string,
         email: string,
-        competence_id: number,
-        years_of_experience: number,
-        from_date: string,
-        to_date: string,
+        competence: Competence[],
+        availability: Availability[],
     ){
         this.person_id = person_id;
         this.name = name;
         this.surname = surname;
         this.email = email;
-        this.competence_id = competence_id;
-        this.years_of_experience = years_of_experience;
-        this.from_date = from_date;
-        this.to_date = to_date;
+        this.competence = competence;
+        this.availability = availability;
     }
 }
 
