349dcbf267f673191bb5e3a393d955cf4f5bc346
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-02-21 21:08:34 +0100

    Use default development ports

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 @echo off
+SET PORT=8080
 start cmd /k "aleph dev recruitment-app"
 FOR /F "tokens=*" %%g IN ('heroku config:get DATABASE_URL -a iv1201-recruitment') do (SET DATABASE_URL=%%g)
+SET PORT=8000
 deno run --allow-net --allow-env --unsafely-ignore-certificate-errors recruitment-api\index.ts
