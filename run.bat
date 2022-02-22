fd99f74e93a5edadc31ab634bec0ad0524df0de7
William <Williamaxb@gmail.com>
2022-02-22 11:37:40 +0100

    Fixed issues from main

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
-start cmd /k "deno run --allow-net recruitment-app\index.ts"
+start cmd /k "aleph dev recruitment-app --allow-net"
 FOR /F "tokens=*" %%g IN ('heroku config:get DATABASE_URL -a iv1201-recruitment') do (SET DATABASE_URL=%%g)
 deno run --allow-net --allow-env --unsafely-ignore-certificate-errors recruitment-api\index.ts
