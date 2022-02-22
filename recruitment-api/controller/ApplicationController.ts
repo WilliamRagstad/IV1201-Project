745235d115b8f230bc2adf8cd3c67a4c9c7ce663
William <Williamaxb@gmail.com>
2022-02-22 10:45:49 +0100

    Refactored function to combine data

--- recruitment-api/controller/ApplicationController.ts
+++ recruitment-api/controller/ApplicationController.ts
@@ -22,7 +22,7 @@ export default class ApplicationController extends IController {
   async get({ response }: Context) {
     const result:Application[] | undefined = await ApplicationController.applicationService.getAll();
     const JSONList =  result!.map(application=>convertApplicationToJSON(application));
-    const JSONData = combineData(JSONList);
+    const JSONData = combineDuplicates(JSONList);
     ok(response, JSONData);
   }
 }
@@ -48,31 +48,64 @@ function convertApplicationToJSON(app: Application){
  * @param JSONList A list with JSON values.
  * @returns The sorted list.
  */
-function combineData(JSONList:any[]){
+function combineDuplicates(JSONList:any[]){
   var JSONData:any[] = [];
 
     for ( var i=0; i<JSONList.length; i++){
-      if(!(JSONData.filter(function(e) {return e.name == JSONList[i].name;}).length>0)){
+      //If object does not already exist in the new list, add it
+      if(compareNames(JSONData, JSONList[i].name)){
           JSONData.push(JSONList[i]);
           JSONData[JSONData.length-1].competences = [];
           JSONData[JSONData.length-1].start = [];
           JSONData[JSONData.length-1].end = [];
       } else {
-          JSONData[JSONData.length-1].competences.push(JSONList[i].competences[0]);
-          if(!JSONData[JSONData.length-1].start.includes(JSONList[i].start[0]))
+        //Add the competences and dates to the final object if a duplicate does not exist
+          if(compareCompetences(JSONData[JSONData.length-1].competences, JSONList[i].competences))
+            JSONData[JSONData.length-1].competences.push(JSONList[i].competences[0]);
+          if(compareDates(JSONData[JSONData.length-1].start, JSONList[i].start))
             JSONData[JSONData.length-1].start.push(JSONList[i].start[0]);
-          if(!JSONData[JSONData.length-1].end.includes(JSONList[i].end[0]))
+          if(compareDates(JSONData[JSONData.length-1].end, JSONList[i].end))
             JSONData[JSONData.length-1].end.push(JSONList[i].end[0]);
       }
-      var compList = [];
-      var checkList:number[] = [];
-      for(var j = 0; j < JSONData[JSONData.length-1].competences.length; j++){
-        if(!checkList.includes(JSONData[JSONData.length-1].competences[j][0])){
-          checkList.push(JSONData[JSONData.length-1].competences[j][0]);
-          compList.push(JSONData[JSONData.length-1].competences[j])
-        }
-      }
-      JSONData[JSONData.length-1].competences = compList;
   }
   return JSONData;
+}
+
+/**
+ * Compares the lists competences to check for duplicates
+ * @param first_list The new list
+ * @param second_list The old list to compare against
+ * @returns True or false if duplicates exist or not
+ */
+function compareCompetences(first_list:any[], second_list:any[]){
+  const duplicates = first_list.filter(
+    function(subList:any[]) {
+      return (subList[0]==second_list[0][0])
+    });
+  return (duplicates.length == 0 ?  true :  false);
+}
+
+/**
+ * Compares the lists dates to check for duplicates
+ * @param first_list The new list
+ * @param second_list The old list to compare against
+ * @returns True or false if duplicates exist or not
+ */
+function compareDates(first_list:any[], second_list:any[]){
+  const duplicates = first_list.filter(
+    function(date:any) {
+      return (date-second_list[0]==0)
+    });
+  return (duplicates.length == 0 ? true : false);
+}
+
+/**
+ * Compares the list against the new name to check for duplicates
+ * @param new_list The list to compare 
+ * @param name The name to compare against
+ * @returns True or false if duplicates exist or not
+ */
+function compareNames(new_list:any[], name:string){
+  const duplicates = new_list.filter(function(obj) {return obj.name == name});
+  return (duplicates.length == 0 ? true : false);
 }
