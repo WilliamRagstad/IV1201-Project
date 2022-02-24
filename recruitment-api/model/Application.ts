d4f42403b42ca50f70b8a514eb758486ba88da52
Ludvig Lindén <ludvig978@gmail.com>
2022-02-24 18:00:24 +0100

    Refactored application model

--- recruitment-api/model/Application.ts
+++ recruitment-api/model/Application.ts
@@ -1,31 +1,20 @@
+import Competence from "./Competence.ts";
+import Availability from "./Availability.ts";
+import User from "./User.ts";
+
 export default class Application {
-    person_id: number;
-    name: string;
-    surname: string;
-    email: string;
-    competence_id: number;
-    years_of_experience: number;
-    from_date: Date;
-    to_date: Date;
+    user: User;
+    competences: Competence[];
+    availability: Availability[];
 
     constructor(
-        person_id: number,
-        name: string,
-        surname: string,
-        email: string,
-        competence_id: number,
-        years_of_experience: number,
-        from_date: Date,
-        to_date: Date,
+        user: User,
+        competences: Competence[],
+        availability: Availability[],
     ){
-        this.person_id = person_id;
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.competence_id = competence_id;
-        this.years_of_experience = years_of_experience;
-        this.from_date = from_date;
-        this.to_date = to_date;
+        this.user = user;
+        this.competences = competences;
+        this.availability = availability;
     }
 }
 
--- recruitment-api/model/Competence.ts
+++ recruitment-api/model/Competence.ts
@@ -4,14 +4,14 @@ import { Optional } from "https://deno.land/x/knight@2.0.3/mod.ts";
  * Competence DTO
  */
 export default class Competence {
-  competence_id: number;
+  id: number;
   years_of_experience: number;
 
   constructor(
-    competence_id: number,
+    id: number,
     years_of_experience: number,
   ) {
-    this.competence_id = competence_id;
+    this.id = id;
     this.years_of_experience = years_of_experience;
   }
 }
