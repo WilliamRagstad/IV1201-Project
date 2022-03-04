7ac7c49be5d6f4e49101b7a7d3d43aa5b79ed3c3
William <Williamaxb@gmail.com>
2022-03-04 08:49:00 +0100

    Combining sections

--- recruitment-app/components/header.tsx
+++ recruitment-app/components/header.tsx
@@ -15,14 +15,13 @@ export default function Header() {
       <p className="nav">
         <a href="/">Home</a>
         <span></span>
-
-        {(title==="other") && (<>
+        {(title==="applicant") && (<><a href="/job">Apply for Job</a><span></span></>) ||
+        (title==="recruiter") && (<><a href="/application">See applications</a><span></span></>) ||
+        (<>
           <a href="/login">Login</a>
           <span></span>
           <a href="/signup">Sign Up</a>
         </>)}
-        {(title==="applicant") && (<><a href="/job">Apply for Job</a><span></span></>)}
-        {(title==="recruiter") && (<><a href="/application">See applications</a><span></span></>)}
         {(title!=="other") && (<><a href="/" onClick={()=>{localStorage.clear();window.location.href="/";}}>Logout</a></>)}
       </p>
     </div>
