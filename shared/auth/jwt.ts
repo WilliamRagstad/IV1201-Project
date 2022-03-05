d26351ae8e6a4958603a9da72eec59f71124d0f6
William Rågstad <william.ragstad@gmail.com>
2022-03-06 00:16:13 +0100

    Refactor JWT logic

--- shared/auth/jwt.ts
+++ shared/auth/jwt.ts
@@ -7,32 +7,18 @@ import {
 } from "https://deno.land/x/djwt@v2.4/mod.ts";
 
 const encoder = new TextEncoder();
-const keyBuf = encoder.encode("IV1201-Project-Key-Buf");
+const keyBuf = encoder.encode("IV1201-Project-Key");
 
 /**
  * Creates a Crypto Key to use for creating and verifying JWTs.
  */
-const key = await crypto.subtle.importKey(
+const key: CryptoKey = await crypto.subtle.importKey(
   "raw",
   keyBuf,
   { name: "HMAC", hash: "SHA-256" },
   true,
   ["sign", "verify"],
 );
-
-/**
- * Creates a payload for each type of user, which lasts 30 minutes.
- * @param role_id Recruiter or Applicant
- * @returns The Payload
- */
-const payloader = (role_id: number) => {
-  let payload: Payload = {
-    iss: "iv1201-api",
-    exp: getNumericDate(60 * 30),
-    user: role_id,
-  };
-  return payload;
-};
 
 /**
  * The header containing the algorithm and type.
@@ -43,18 +29,32 @@ const header: Header = {
 };
 
 /**
+ * Creates a payload for each type of user, which lasts 30 minutes.
+ * @param role_id Recruiter or Applicant
+ * @returns The Payload
+ */
+const payloader = (data: any): Payload => {
+  return {
+    iss: "iv1201-jwt",
+    exp: getNumericDate(60 * 30),
+    data: data,
+  };
+};
+
+/**
  * Verifies a JWT against a crypto key.
- * @param token The token to be verified
+ * @param token The token containing the JWT
  * @returns The payload of the token.
  */
-export async function verifyJWT(token: string) {
+export async function verifyJWT(token: string): Promise<Payload> {
   return verify(token, key);
 }
+
 /**
  * Creates a JWT with a payload based on the users role.
- * @param role_id The id of the role.
- * @returns A JWT.
+ * @param payload The payload of the JWT.
+ * @returns A new JWT.
  */
-export async function createJWT(payload: any) {
+export async function createJWT(payload: any): Promise<string> {
   return create(header, payloader(payload), key);
 }
