8d901c235e433961664d7b23fb14feb9d18ab9d2
William Rågstad <william.ragstad@gmail.com>
2022-03-06 00:20:38 +0100

    Use SSR options

--- recruitment-app/lib/verification.ts
+++ recruitment-app/lib/verification.ts
@@ -1,26 +1,41 @@
+import { useDeno } from "aleph/react";
+import { SSROptions } from "aleph/types";
+// import { getCookies } from "https://deno.land/std/http/cookie.ts";
+// import { verifyJWT } from "~/lib/shared/auth/jwt.ts";
+
+// Default JWT handler for SSR
+let getUser = async () => undefined;
+
+if (window.Deno != undefined) {
+  // Server side
+  const cwd = Deno.cwd();
+  const app = cwd.endsWith("recruitment-app")
+    ? cwd.substring(0, cwd.indexOf("recruitment-app"))
+    : cwd;
+  const { verifyJWT } = await import(`file://${app}/shared/auth/jwt.ts`);
+  const { getCookies } = await import(`https://deno.land/std/http/cookie.ts`);
+  getUser = async (req: any) => {
+    try {
+      return await verifyJWT(getCookies(req.headers).JWT).data;
+    } catch (e: any) {
+      return undefined;
+    }
+  };
+}
+
 /**
- * Verifies the users JWT to check authorization privileges. 
- * @returns The users role title. 
+ * Import and export the `ssr` options from every page that requires access to the current user session.
  */
-export const fetchJWTAuthorization = async () => {
-    var title="other";
-      if(localStorage.length > 0 && title=="other"){
-        await fetch("http://localhost:8000/user/verify/"+localStorage.getItem("JWT")+"/")
-          .then((res) => {
-            if(!res.ok)
-              throw Error(res.statusText);
-            else
-              return res.json()
-          })
-          .then(data => {
-            if(data.user==1)
-              title="recruiter";
-            else if(data.user==2)
-              title="applicant";
-          })
-          .catch((error)=>{
-            title="other";
-        })
-      } 
-    return title;
+export const ssr: SSROptions = {
+  props: async (req) => {
+    console.log(req);
+    return {
+      $revalidate: 0, // revalidate props for every request
+      user: await getUser(req), // Save JWT payload in user property
+      serverTime: Date.now(),
+    };
+  },
+  paths: async () => {
+    return [];
+  },
 };
