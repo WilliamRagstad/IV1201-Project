9a449634b83975caf48245002d02be659f9f8d3e
William Rågstad <william.ragstad@gmail.com>
2022-03-06 00:28:18 +0100

    Fix application page and use SSR `user` page property

--- recruitment-app/pages/application.tsx
+++ recruitment-app/pages/application.tsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
 import DefaultPage from "~/components/defaultPage.tsx";
 import { useDeno } from "aleph/react";
-import { fetchJWTAuthorization } from "../lib/verification.ts"
-const title = await fetchJWTAuthorization();
+export { ssr } from "~/lib/verification.ts";
 
 /**
  * The admin page for recruiters to see the applications
  * @returns A page with applications
  */
-export default function Application() {
+export default function Application({ user }: any) {
   /**
    * Retrieves and formats the user data.
    */
@@ -154,122 +153,131 @@ export default function Application() {
   }
 
   return (
-    <DefaultPage header="View Applications">
-      {console.log(title)}
-      {(userData.message && (
-        <h2 className="error-message">{userData.message}</h2>
-      )) ||
-      (title==("applicant"||"other"))&&(<><p>Error 401 - Unauthorized</p></>) || 
-      (title=="recruiter") && (
-        <>
-          <div className="search flex-parent">
-            <div className="flex-child">
-              <label className="txt_field" htmlFor="search_name">
-                Search for name:
-              </label>
+    <DefaultPage header="View Applications" user={user}>
+      {userData.message
+        ? <h2 className="error-message">{userData.message}</h2>
+        : user.role.name === "recruiter"
+        ? (
+          <>
+            <div className="search flex-parent">
+              <div className="flex-child">
+                <label className="txt_field" htmlFor="search_name">
+                  Search for name:
+                </label>
+                <input
+                  type="text"
+                  id="search_name"
+                  onChange={updateSearchCriteria}
+                />
+              </div>
+              <div className="flex-child">
+                <label className="txt_field" htmlFor="search_competence">
+                  Search for competence:
+                </label>
+                <select
+                  id="search_competence"
+                  onChange={updateSearchCriteria}
+                >
+                  <option value="-1">All</option>
+                  <option value="1">Ticket Sales</option>
+                  <option value="2">Lotteries</option>
+                  <option value="3">Roller Coaster Operation</option>
+                </select>
+              </div>
+              <div className="flex-child">
+                <label htmlFor="from_date">Available between dates:</label>
+                <input
+                  type="date"
+                  id="from_date"
+                  onChange={updateSearchCriteria}
+                />
+                <label htmlFor="to_date">and:</label>
+                <input
+                  type="date"
+                  id="to_date"
+                  onChange={updateSearchCriteria}
+                />
+              </div>
               <input
-                type="text"
-                id="search_name"
-                onChange={updateSearchCriteria}
+                className="button"
+                type="button"
+                value="Search"
+                onClick={updateSearchCriteria}
               />
             </div>
-            <div className="flex-child">
-              <label className="txt_field" htmlFor="search_competence">
-                Search for competence:
-              </label>
-              <select id="search_competence" onChange={updateSearchCriteria}>
-                <option value="-1">All</option>
-                <option value="1">Ticket Sales</option>
-                <option value="2">Lotteries</option>
-                <option value="3">Roller Coaster Operation</option>
-              </select>
-            </div>
-            <div className="flex-child">
-              <label htmlFor="from_date">Available between dates:</label>
-              <input
-                type="date"
-                id="from_date"
-                onChange={updateSearchCriteria}
-              />
-              <label htmlFor="to_date">and:</label>
-              <input type="date" id="to_date" onChange={updateSearchCriteria} />
-            </div>
-            <input
-              className="button"
-              type="button"
-              value="Search"
-              onClick={updateSearchCriteria}
-            />
-          </div>
-          {currentPageApplications.length > 0 && (
-                <>
-                  <div className="applications">
-                    <ul className="user_list">
-                      {currentPageApplications.map((app: any, i) => (
-                        <li
-                          className="user_box"
-                          onClick={() => setCurrentApplication(app)}
-                          key={"u" + i}
-                        >
-                          {app.user.firstName + " " + app.user.lastName}
-                        </li>
-                      ))}
-                    </ul>
+            {currentPageApplications.length > 0 && (
+                  <>
+                    <div className="applications">
+                      <ul className="user_list">
+                        {currentPageApplications.map((app: any, i) => (
+                          <li
+                            className="user_box"
+                            onClick={() => setCurrentApplication(app)}
+                            key={"u" + i}
+                          >
+                            {app.user.firstName + " " + app.user.lastName}
+                          </li>
+                        ))}
+                      </ul>
 
-                    <div className="application_page">
-                      <div>
-                        <p>
-                          Name: {currentApplication.user.firstName + " " +
-                            currentApplication.user.lastName}
-                        </p>
-                        <p>Email: {currentApplication.user.email}</p>
-                        <ul className="application_list">
-                          Availability periods:
-                          {currentApplication.availability.map((
-                            availability: any,
-                            i: number,
-                          ) => (
-                            <li key={"d" + i} className="list_box">
-                              {new Date(availability.start_date)
-                                .toLocaleDateString()} to{" "}
-                              {new Date(availability.end_date)
-                                .toLocaleDateString()}
-                            </li>
-                          ))}
-                        </ul>
-                        <ul className="application_list">
-                          Competences:
-                          {currentApplication.competences.map(
-                            (comp: any, i: number) => (
-                              <li key={"c" + i} className="list_box">
-                                {listCompetence(comp)}
+                      <div className="application_page">
+                        <div>
+                          <p>
+                            Name: {currentApplication.user.firstName + " " +
+                              currentApplication.user.lastName}
+                          </p>
+                          <p>Email: {currentApplication.user.email}</p>
+                          <ul className="application_list">
+                            Availability periods:
+                            {currentApplication.availability.map((
+                              availability: any,
+                              i: number,
+                            ) => (
+                              <li key={"d" + i} className="list_box">
+                                {new Date(availability.start_date)
+                                  .toLocaleDateString()} to{" "}
+                                {new Date(availability.end_date)
+                                  .toLocaleDateString()}
                               </li>
-                            ),
-                          )}
-                        </ul>
+                            ))}
+                          </ul>
+                          <ul className="application_list">
+                            Competences:
+                            {currentApplication.competences.map(
+                              (comp: any, i: number) => (
+                                <li key={"c" + i} className="list_box">
+                                  {listCompetence(comp)}
+                                </li>
+                              ),
+                            )}
+                          </ul>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="user_arrow_box">
-                    <li
-                      className="user_arrow"
-                      onClick={() => setPageIndex(pageIndex - 1)}
-                    >
-                      Previous Page
-                    </li>
-                    <li
-                      className="user_arrow"
-                      onClick={() => setPageIndex(pageIndex + 1)}
-                    >
-                      Next Page
-                    </li>
-                  </div>
-                </>
-              ) || (
-            <h2 className="error-message">No users matching search criteria</h2>
-          )}
-        </>
-      )}
+                    <div className="user_arrow_box">
+                      <li
+                        className="user_arrow"
+                        onClick={() => setPageIndex(pageIndex - 1)}
+                      >
+                        Previous Page
+                      </li>
+                      <li
+                        className="user_arrow"
+                        onClick={() => setPageIndex(pageIndex + 1)}
+                      >
+                        Next Page
+                      </li>
+                    </div>
+                  </>
+                ) || (
+              <h2 className="error-message">
+                No users matching search criteria
+              </h2>
+            )}
+          </>
+        )
+        : <p>Error 401 - Unauthorized</p> //! TODO: Don't manage routing in page components!!!
+        }
     </DefaultPage>
   );
 }
