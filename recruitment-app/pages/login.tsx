be9c6ffbbe71f245dec97bcb1ed3c64b0e9c3ff6
William Rågstad <william.ragstad@gmail.com>
2022-03-06 00:30:37 +0100

    Refactor logic and use SSR `user` page property

--- recruitment-app/pages/login.tsx
+++ recruitment-app/pages/login.tsx
@@ -1,62 +1,72 @@
-import React, { useState } from "react";
+import React, { FormEventHandler, useState } from "react";
 import DefaultPage from "~/components/defaultPage.tsx";
 import { hashPassword } from "../lib/passhash.ts";
-import { fetchJWTAuthorization } from "../lib/verification.ts"
-const title = await fetchJWTAuthorization();
+export { ssr } from "~/lib/verification.ts";
 
 /**
  * The Login page with a login form.
  * @returns The Login screen component.
  */
-export default function Login() {
+export default function Login({ user }: { user: any }) {
+  console.log(user ?? "user is undefined");
   const [error, setError] = useState("");
 
   /**
    * Function to perform extra manipulation before sending formdata.
    */
-  const submitForm = async function () {
-    const password =
-    (document.getElementById("password") as HTMLInputElement).value;
-    const hashed = hashPassword(password);
-    (document.getElementById("password") as HTMLInputElement).value = hashed;
-    
-    const data = new URLSearchParams();
-    for (const pair of new FormData(document.getElementById('myForm') as HTMLFormElement | undefined)){
-      data.append(pair[0], pair[1] as string);
-    }
+  const submitForm: FormEventHandler = async function (event: React.FormEvent) {
+    event.preventDefault();
+    setError("");
+    const emailElm = document.getElementById("email") as HTMLInputElement;
+    const passwordElm = document.getElementById("password") as HTMLInputElement;
+    const password = passwordElm.value;
+    const hashedPassword = hashPassword(password);
+
     //TODO: Make POST request instead (data as body)
-    await fetch("http://localhost:8000/user/validate/"+data.get("email")+"/"+data.get("password"))
-    .then(response => response.text())
-    .then(data => {
-      if(data){
-        localStorage.setItem("JWT", data);
-        window.location.href="/";
-      } else{
-        (document.getElementById("password") as HTMLInputElement).value = ""
-        setError("Wrong username or password, try again");
-      }
-    })
-    .catch(error => console.log("Try Again"));
+    const res = await fetch(
+      `http://localhost:8000/user/validate/${emailElm.value}/${hashedPassword}`,
+    );
+    const jwt = await res.text();
+    if (jwt) {
+      document.cookie = `JWT=${jwt}`;
+      window.location.href = "/";
+    } else {
+      passwordElm.value = "";
+      setError("Wrong username or password, try again");
+    }
   };
 
   return (
-    <DefaultPage header="Login to site">
-      {(title==="other")&&(<><form
-        id="myForm"
-        className="signup_form"
-        encType="multipart/form-data"
-      >
-        Email:
-        <label className="txt_field">
-          <input type="email" name="email" />
-        </label>
-        Password:
-        <label className="txt_field">
-          <input id="password" type="password" name="password" />
-        </label>
-      </form>
-      <button className="button" onClick={submitForm}>Login</button></>)
-      ||(<><p>Already logged in</p></>)}
+    <DefaultPage header="Login to site" user={user}>
+      {!user && (
+            <>
+              <form
+                id="myForm"
+                className="signup_form"
+                encType="multipart/form-data"
+                method="POST"
+                onSubmit={submitForm}
+              >
+                Email:
+                <label className="txt_field">
+                  <input id="email" type="email" name="email" />
+                </label>
+                Password:
+                <label className="txt_field">
+                  <input id="password" type="password" name="password" />
+                </label>
+                <label>
+                  {error && <p>{error}</p>}
+                </label>
+                <button type="submit" className="button">Login</button>
+              </form>
+            </>
+          ) ||
+        (
+          <>
+            <p>Already logged in</p>
+          </>
+        )}
     </DefaultPage>
   );
 }
