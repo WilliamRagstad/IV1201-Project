4a1c378fd38577e036f907f7dbd27a8fa76aa514
William Rågstad <william.ragstad@gmail.com>
2022-03-06 00:15:56 +0100

    Adapted to `user` property pattern, rest needs fixing

--- recruitment-app/pages/signup.tsx
+++ recruitment-app/pages/signup.tsx
@@ -1,63 +1,68 @@
 import React, { useEffect, useState } from "react";
 import DefaultPage from "../components/defaultPage.tsx";
 import { hashPassword } from "../lib/passhash.ts";
-import { fetchJWTAuthorization } from "../lib/verification.ts"
-const title = await fetchJWTAuthorization();
+export { ssr } from "~/lib/verification.ts";
 
 /**
  * The signup page.
  * @returns a page with the form to register to the recruitment app.
  */
-export default function Signup() {
+export default function Signup({ user }: any) {
   /**
    * Function to perform extra manipulation before sending formdata.
    */
   const submitForm = function () {
-    const password =
-      (document.getElementById("password") as HTMLInputElement).value;
-    const hashed = hashPassword(password);
-    (document.getElementById("password") as HTMLInputElement).value = hashed;
+    const passwordElm = document.getElementById("password") as HTMLInputElement;
+    const password = passwordElm.value;
+    const hashedPassword = hashPassword(password);
+    passwordElm.value = hashedPassword;
   };
-  
-  return (
-    <DefaultPage header="Sign up">
-      {(title=="other")&&(<form
-        action="http://localhost:8000/user"
-        method="post"
-        className="signup_form"
-        encType="multipart/form-data"
-        onSubmit={submitForm}
-      >
-        <div className="personal_information">
-          First Name:
-          <label className="txt_field">
-            <input type="text" name="firstName" />
-          </label>
-          Last Name:
-          <label className="txt_field">
-            <input type="text" name="lastName" />
-          </label>
-          Person Number:
-          <label className="txt_field">
-            <input type="number" name="socialSecurityNumber" />
-          </label>
-          Email:
-          <label className="txt_field">
-            <input type="email" name="email" />
-          </label>
-          Username:
-          <label className="txt_field">
-            <input type="text" name="username" />
-          </label>
-          Password:
-          <label className="txt_field">
-            <input id="password" type="password" name="password" />
-          </label>
-        </div>
 
-        <input type="submit" value="Sign Up" className="button" />
-      </form>)
-      ||(<><p>Already signed in</p></>)}
+  return (
+    <DefaultPage header="Sign up" user={user}>
+      {!user && (
+            <form
+              action="http://localhost:8000/user"
+              method="post"
+              className="signup_form"
+              encType="multipart/form-data"
+              onSubmit={submitForm}
+            >
+              <div className="personal_information">
+                First Name:
+                <label className="txt_field">
+                  <input type="text" name="firstName" />
+                </label>
+                Last Name:
+                <label className="txt_field">
+                  <input type="text" name="lastName" />
+                </label>
+                Person Number:
+                <label className="txt_field">
+                  <input type="number" name="socialSecurityNumber" />
+                </label>
+                Email:
+                <label className="txt_field">
+                  <input type="email" name="email" />
+                </label>
+                Username:
+                <label className="txt_field">
+                  <input type="text" name="username" />
+                </label>
+                Password:
+                <label className="txt_field">
+                  <input id="password" type="password" name="password" />
+                </label>
+              </div>
+
+              <input type="submit" value="Sign Up" className="button" />
+            </form>
+          ) ||
+        (
+          <>
+            <p>Already signed in</p>
+          </>
+        )}
     </DefaultPage>
   );
 }
