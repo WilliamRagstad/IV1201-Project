91ec05023833612c44471aa6b361b785e22b33ec
William Rågstad <william.ragstad@gmail.com>
2022-03-06 00:29:59 +0100

    Use SSR `user` page property, fix routing somewhere else than in pages

--- recruitment-app/pages/job.tsx
+++ recruitment-app/pages/job.tsx
@@ -1,66 +1,70 @@
 import React, { useEffect, useState } from "react";
 import DefaultPage from "~/components/defaultPage.tsx";
-import { fetchJWTAuthorization } from "../lib/verification.ts"
-const title = await fetchJWTAuthorization();
+export { ssr } from "~/lib/verification.ts";
 
 /**
  * The Job page to apply for work.
  * @returns A page with the forms to select work competence and the availability dates.
  */
-export default function Job() {
-  
+export default function Job({ user }: any) {
   return (
-    <DefaultPage header="Apply for work here">
-      {(title==="applicant")&&
-      (<form action="/" method="post" className="signup_form flex-parent">
-        <div>
-          <h2>Choose your expertise</h2>
-          <div className="flex-parent">
+    <DefaultPage header="Apply for work here" user={user}>
+      {user && user.role.name === "applicant"
+        ? (
+          <form action="/" method="post" className="signup_form flex-parent">
             <div>
-              <input
-                className="check_box"
-                type="checkbox"
-                name="expertise1"
-                id="expertise1"
-              />
-              <label htmlFor="expertise1">Ticket sales</label>
+              <h2>Choose your expertise</h2>
+              <div className="flex-parent">
+                <div>
+                  <input
+                    className="check_box"
+                    type="checkbox"
+                    name="expertise1"
+                    id="expertise1"
+                  />
+                  <label htmlFor="expertise1">Ticket sales</label>
+                </div>
+                <div>
+                  <input
+                    className="check_box"
+                    type="checkbox"
+                    name="expertise2"
+                    id="expertise2"
+                  />
+                  <label htmlFor="expertise2">Lotteries</label>
+                </div>
+                <div>
+                  <input
+                    className="check_box"
+                    type="checkbox"
+                    name="expertise3"
+                    id="expertise3"
+                  />
+                  <label htmlFor="expertise3">Roller coaster operation</label>
+                </div>
+              </div>
             </div>
-            <div>
-              <input
-                className="check_box"
-                type="checkbox"
-                name="expertise2"
-                id="expertise2"
-              />
-              <label htmlFor="expertise2">Lotteries</label>
+            <div className="availability_dates">
+              <h2>Dates for work:</h2>
+              <div className="flex-parent">
+                <div>
+                  <label htmlFor="startdate">From:</label>
+                  <input type="date" name="startdate" id="startdate" />
+                </div>
+                <div>
+                  <label htmlFor="enddate">To:</label>
+                  <input type="date" name="enddate" id="enddate" />
+                </div>
+              </div>
             </div>
-            <div>
-              <input
-                className="check_box"
-                type="checkbox"
-                name="expertise3"
-                id="expertise3"
-              />
-              <label htmlFor="expertise3">Roller coaster operation</label>
-            </div>
-          </div>
-        </div>
-        <div className="availability_dates">
-          <h2>Dates for work:</h2>
-          <div className="flex-parent">
-            <div>
-              <label htmlFor="startdate">From:</label>
-              <input type="date" name="startdate" id="startdate" />
-            </div>
-            <div>
-              <label htmlFor="enddate">To:</label>
-              <input type="date" name="enddate" id="enddate" />
-            </div>
-          </div>
-        </div>
-        <input type="submit" value="Apply" className="button" />
-      </form>)||
-      (<><p>Error 401 - Unauthorized</p></>)}
+            <input type="submit" value="Apply" className="button" />
+          </form>
+        )
+        : (
+          <>
+            <p>Error 401 - Unauthorized</p> //! TODO: Don't manage routing in page components!!!
+          </>
+        )}
     </DefaultPage>
   );
 }
