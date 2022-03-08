def650dd3f8c80e859814ad24d5b1dbea2ce8395
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-08 14:01:35 +0100

    Update login.tsx

--- recruitment-app/pages/login.tsx
+++ recruitment-app/pages/login.tsx
@@ -1,7 +1,6 @@
 import React, { FormEventHandler, useState } from "react";
 import { useDeno } from "aleph/react";
 import DefaultPage from "~/components/defaultPage.tsx";
-import { useDeno } from "aleph/react";
 import { hashPassword } from "../lib/passhash.ts";
 export { ssr } from "~/lib/verification.ts";
 
