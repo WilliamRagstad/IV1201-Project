b7d1f9cbb65316bcba452c525c4c16ea29ae757e
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-08 21:30:45 +0100

    Create api.ts

---
+++ recruitment-app/lib/api.ts
@@ -0,0 +1,22 @@
+// Possible API base URLs:
+const baseURLs = [
+  "http://localhost:8000/",
+  "https://iv1201-recruitment-api.herokuapp.com/",
+];
+
+/**
+ * Fetch data from the local or public API.
+ * @param endpoint Name of the endpoint without the base url.
+ */
+export async function getAPI(endpoint: string) {
+  const results = await Promise.allSettled(
+    baseURLs.map((b) => fetch(b + endpoint)),
+  );
+  return (results.find((result) => result.status === "fulfilled") as
+    | PromiseFulfilledResult<Response>
+    | undefined)?.value ??
+    new Response(null, {
+      status: 404,
+      statusText: "Request failed, endpoint not found",
+    });
+}
