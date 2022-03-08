a5b686f0da0e367c7a2694d61687d55dc760e59d
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-08 13:35:09 +0100

    Delete error.tsx

--- recruitment-app/pages/error.tsx
+++
@@ -1,7 +0,0 @@
-import DefaultPage from '~/components/defaultPage.tsx';
-
-export default function ErrorPage({ title, message, user }) {
-    return <DefaultPage header={title} user={user}>
-        <p>{message}</p>
-    </DefaultPage>;
-}
