5b9d07dd9b196536ed3d2f11b7c2b6581bea256b
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-08 12:44:13 +0100

    Update verification.ts

--- recruitment-app/lib/verification.ts
+++ recruitment-app/lib/verification.ts
@@ -1,26 +1,13 @@
-import { useDeno } from "aleph/react";
 import { SSROptions } from "aleph/types";
 // import { getCookies } from "https://deno.land/std/http/cookie.ts";
 // import { verifyJWT } from "~/lib/shared/auth/jwt.ts";
 
-// Default JWT handler for SSR
-let getUser = async () => undefined;
-
-if (window.Deno != undefined) {
-  // Server side
-  const cwd = Deno.cwd();
-  const app = cwd.endsWith("recruitment-app")
-    ? cwd.substring(0, cwd.indexOf("recruitment-app"))
-    : cwd;
-  const { verifyJWT } = await import(`file://${app}/shared/auth/jwt.ts`);
-  const { getCookies } = await import(`https://deno.land/std/http/cookie.ts`);
-  getUser = async (req: any) => {
-    try {
-      return await verifyJWT(getCookies(req.headers).JWT).data;
-    } catch (e: any) {
-      return undefined;
-    }
-  };
+function appRootPath() {
+	const cwd = Deno.cwd();
+    const app = cwd.endsWith("recruitment-app")
+      ? cwd.substring(0, cwd.indexOf("recruitment-app"))
+      : cwd;
+	return app;
 }
 
 /**
@@ -28,14 +15,22 @@ if (window.Deno != undefined) {
  */
 export const ssr: SSROptions = {
   props: async (req) => {
-    console.log(req);
+    // console.log("Loading current user...");
+    const { verifyJWT } = await import(`file://${appRootPath()}/shared/auth/jwt.ts`);
+    const { getCookies } = await import(`https://deno.land/std/http/cookie.ts`);
+    let payload = undefined;
+    try {
+      const jwt = await verifyJWT(getCookies(req.headers).JWT);
+      payload = jwt.data;
+    } catch { /* Ignore */ }
+    // console.log("Current user:", payload);
     return {
       $revalidate: 0, // revalidate props for every request
-      user: await getUser(req), // Save JWT payload in user property
+      user: payload, // Save JWT payload in user property
       serverTime: Date.now(),
     };
   },
-  paths: async () => {
+  paths: () => {
     return [];
   },
 };
