85cf15ed30e31cd1467438a136bbb4bb6ddca005
William <Williamaxb@gmail.com>
2022-03-09 14:39:55 +0100

    Remove unused imports

--- recruitment-app/pages/signup.tsx
+++ recruitment-app/pages/signup.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useDeno } from "aleph/react";
 import DefaultPage from "../components/defaultPage.tsx";
 import { hashPassword } from "../lib/passhash.ts";
