2c5ddaec46ad19ef2f04a49a439bb03813dc7f48
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-10 00:52:26 +0100

    Update LoggingService.ts

--- recruitment-api/service/LoggingService.ts
+++ recruitment-api/service/LoggingService.ts
@@ -1,22 +1,23 @@
 import {
-	ConsoleSink,
-	FileSink,
-	Logger,
-	LoggingLevel,
-	Service,
-  } from "https://deno.land/x/knight@2.2.0/mod.ts";
+  ConsoleSink,
+  FileSink,
+  Logger,
+  LoggingLevel,
+  Service,
+} from "https://deno.land/x/knight@2.2.0/mod.ts";
 
-  export default Service(
-	class LoggingService {
-	  public logger: Logger;
-	  constructor() {
-		this.logger = new Logger()
-		  .attach(new ConsoleSink())
-		  .attach(
-			new FileSink("./logs/log.txt").fromRange(
-			  LoggingLevel.Success,
-			),
-		  );
-	  }
-	},
-  );
+export default Service(
+  class LoggingService {
+    public logger: Logger;
+    constructor() {
+      this.logger = new Logger()
+        .attach(new ConsoleSink())
+        .attach(
+          new FileSink("./logs/log.txt").fromRange(
+            // Log everything from the warning level and above to a file
+            LoggingLevel.Warning,
+          ),
+        );
+    }
+  },
+);
