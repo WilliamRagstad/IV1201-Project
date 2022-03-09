9de3c97ffe62fa9cfbd88425c24584f137da719e
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-10 00:56:28 +0100

    Convert to Knight Service

--- recruitment-api/service/ApplicationService.ts
+++ recruitment-api/service/ApplicationService.ts
@@ -1,35 +1,26 @@
+import { Service } from "https://deno.land/x/knight@2.2.0/mod.ts";
 import Application from "../model/Application.ts";
 import ApplicationRepository from "../repository/ApplicationRepository.ts";
 
 /**
  * Application service class.
  */
-export default class ApplicationService {
-  private static instance: ApplicationService;
-  applicationRepository: ApplicationRepository;
-  constructor() {
-    this.applicationRepository = ApplicationRepository.getInstance();
-  }
-
-  /**
-   * Get the user service singleton instance.
-   * @returns The singleton instance.
-   */
-  public static getInstance(): ApplicationService {
-    if (!ApplicationService.instance) {
-      ApplicationService.instance = new ApplicationService();
+export default Service(
+  class ApplicationService {
+    applicationRepository: ApplicationRepository;
+    constructor() {
+      this.applicationRepository = ApplicationRepository.getInstance();
     }
-    return ApplicationService.instance;
-  }
 
-  /**
-   * Retrieves all applications
-   * @returns All Applications
-   */
-  async getAll(): Promise<Application[] | undefined> {
-    return await this.applicationRepository.db.useTransaction<Application[]>(
-      "transaction_applications_get",
-      async (t) => await this.applicationRepository.getAll(t),
-    );
-  }
-}
+    /**
+     * Retrieves all applications
+     * @returns All Applications
+     */
+    async getAll(): Promise<Application[] | undefined> {
+      return await this.applicationRepository.db.useTransaction<Application[]>(
+        "transaction_applications_get",
+        async (t) => await this.applicationRepository.getAll(t),
+      );
+    }
+  },
+);
