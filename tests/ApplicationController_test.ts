7eea64bb389ecb7154dfea3a7b717def9dd956a4
Ludvig Lindén <ludvig978@gmail.com>
2022-03-09 10:56:58 +0100

    Changed applicationcontroller test to work with new applicationmodel

--- tests/ApplicationController_test.ts
+++ tests/ApplicationController_test.ts
@@ -1,18 +1,21 @@
-import { assertEquals, assertNotEquals, assert } from "https://deno.land/std@0.126.0/testing/asserts.ts";
+import {
+  assert,
+  assertEquals,
+  assertNotEquals,
+} from "https://deno.land/std@0.126.0/testing/asserts.ts";
 
 /**
  * Connects to the API and retrieves data.
  */
-Deno.test("Application Controller Fetch Attempt", async ()=>{
-    var response:any;
-    await fetch(`http://localhost:8000/application`)
-    .then(res => res.text())
-    .then(data => response = JSON.parse(data)[0])
-    console.log(response);
-    assert(response.person_id)
-    assert(response.name)
-    assert(response.start)
-    assert(response.end)
-    assert(response.competences)
-    assert(response.email)
+Deno.test("Application Controller Fetch Attempt", async () => {
+  var response = await fetch(`http://localhost:8000/application`)
+    .then((res) => res.json())
+    .catch((error) => {
+      return {
+        message: "No connection to the API, try again later.",
+      };
+    });
+  assert(response[0].user);
+  assert(response[0].availability);
+  assert(response[0].competences);
 });
