5e5026c564f1d45d5683d10720db71a275baa7f9
Ludvig Lindén <ludvig978@gmail.com>
2022-03-09 11:20:56 +0100

    Update usercontroller tests to check validate endpoint

--- tests/UserController_test.ts
+++ tests/UserController_test.ts
@@ -1,12 +1,30 @@
-import { assertEquals, assertNotEquals } from "https://deno.land/std@0.126.0/testing/asserts.ts";
+import {
+  assert,
+  assertEquals,
+  assertNotEquals,
+} from "https://deno.land/std@0.126.0/testing/asserts.ts";
+import { Hash, Salt } from "../shared/auth/password_hashing.ts";
 
 /**
- * Connects to the API and retrieves data.
+ * Tries to validate an existing user with the API.
  */
-Deno.test("User Controller Email Fetch Attempt", async ()=>{
-    var response:any;
-    await fetch(`http://localhost:8000/user/test/email`)
-    .then(res => res.text())
-    .then(data => response = data);
-    assertEquals(response, "User with email test@example.com was successfully found");
+Deno.test("User Controller existing user Validate Fetch Attempt", async () => {
+  const password = Hash("hunter2");
+  var response = await fetch(
+    `http://localhost:8000/user/validate/waxbrink@kth.se/${password}`,
+  )
+    .then(async (res) => await res.text());
+  assert(response);
+});
+
+/**
+ * Tries to validate a nonexisting user with the API.
+ */
+Deno.test("User Controller nonexisting user Validate Fetch Attempt", async () => {
+  const password = Hash("test");
+  var response = await fetch(
+    `http://localhost:8000/user/validate/test@kth.se/${password}`,
+  );
+  await response.text()
+  assertEquals(response.status, 404);
 });
