d14e297f759b25d81e7835c16453f31901062426
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-10 01:27:38 +0100

    Add logging to ApplicationController

--- recruitment-api/controller/ApplicationController.ts
+++ recruitment-api/controller/ApplicationController.ts
@@ -17,7 +17,14 @@ export default class ApplicationController extends IController {
   private log = LoggingService.instance().logger;
 
   async get({ response }: Context) {
+    this.log.debug("Request to: GET /application");
+    const applications = await ApplicationController.applicationService.getAll();
+    if (applications) {
+      this.log.success(`Successfully retrieved ${applications.length} applications`);
+    } else {
+      this.log.error("Failed to retrieve applications");
+    }
     response.headers.set("Access-Control-Allow-Origin", "*");
-    ok(response, await ApplicationController.applicationService.getAll());
+    ok(response, applications);
   }
 }
