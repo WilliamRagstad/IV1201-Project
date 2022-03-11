9cc5975f341d2766d81853f5efc7354ed1f194c1
William <Williamaxb@gmail.com>
2022-03-11 14:52:06 +0100

    WIP: Cookie tests

---
+++ recruitment-app/tests/cookies_test.ts
@@ -0,0 +1,17 @@
+import { assertEquals, assertNotEquals } from "https://deno.land/std@0.126.0/testing/asserts.ts";
+
+/**
+ * Connects to the API and retrieves data.
+ */
+Deno.test({
+    name: "deleteAllCookies test",
+    fn(){      
+        /*window.document.cookie ="name=test;"
+        deleteAllCookies();
+        assertEquals(window.document.cookie, "");
+        */
+        assertEquals(true, true);
+    }
+});
+
+
