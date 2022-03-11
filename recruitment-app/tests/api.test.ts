0e4304e934afbd990c5b308bb7bea02a3fda8ad1
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-03-11 15:27:48 +0100

    Refactor

--- recruitment-app/tests/api.test.ts
+++ recruitment-app/tests/api.test.ts
@@ -1,17 +1,15 @@
-import { getAPI } from "../lib/api.ts"; 
-import { assertEquals, assertNotEquals } from "https://deno.land/std@0.126.0/testing/asserts.ts";
+import { getAPI } from "../lib/api.ts";
+import { assertEquals } from "https://deno.land/std@0.126.0/testing/asserts.ts";
 
 /**
  * Connects to the API and retrieves data.
  */
 Deno.test({
-    name: "getAPI test",
-    async fn(){
-        const res = await getAPI('user/test/email');
-        assertEquals(res.ok, true);
-    },
-    sanitizeOps: false,
-    sanitizeResources: false,
+  name: "getAPI test",
+  async fn() {
+    const res = await getAPI("user/test/email");
+    assertEquals(res.ok, true);
+  },
+  sanitizeOps: false,
+  sanitizeResources: false,
 });
-
-
