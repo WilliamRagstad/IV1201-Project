ff888d0e5e437c04719e5238e83326a5ee82e1af
Ludvig Lindén <ludvig978@gmail.com>
2022-03-11 13:53:48 +0100

    Changed order

--- recruitment-app/pages/login.tsx
+++ recruitment-app/pages/login.tsx
@@ -23,7 +23,7 @@ export default function Login({ user }: { user: any }) {
     const emailElm = document.getElementById("email") as HTMLInputElement;
     const passwordElm = document.getElementById("password") as HTMLInputElement;
     const password = passwordElm.value;
-    const hashedPassword = hashPassword(Salt + password);
+    const hashedPassword = hashPassword(password + Salt);
 
     //TODO: Make POST request instead (data as body)
     const res = await getAPI(
